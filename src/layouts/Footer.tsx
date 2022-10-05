1c57e6b5239d8ddbe3afca3795df33974326f50d
whit33y <troppout@gmail.com>
2022-10-05 11:18:33 +0200

    Font change and style changes

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 function Footer() {
-    const [displayNewsletter, setDisplayNewsletter] = useState<boolean>(false)
-    function handleClick() {
-        console.log('newsletter')
-    }
     return (
         <footer className="flex justify-between items-end h-1/6 border-t-2 bg-slate-200 mt-auto ">
             <div>
-                <button className="border-2 border-black px-2 bg-emerald-400 hover:text-emerald-400 hover:bg-black duration-300" onClick={handleClick}>Newsletter</button>
+                <button className="border-2 border-black px-2 bg-emerald-400 hover:text-emerald-400 hover:bg-black duration-300 sm:mb-0 sm:ml-0 md:mb-2 md:ml-2">Newsletter</button>
             </div>
-            <div className="flex">
+            <div className="flex sm:mb-0 md:mb-2 sm:mr-0 md:mr-2">
                 <a href='https://www.facebook.com'><img className='w-6 h-6 mx-1' src='https://cdn-icons-png.flaticon.com/512/4494/4494464.png' /></a>
                 <a href='https://www.instagram.com'><img className='w-6 h-6 mx-1' src='https://cdn-icons-png.flaticon.com/512/1384/1384015.png' /></a>
                 <a href='https://www.twitter.com'><img className='w-6 h-6 mx-1' src='https://cdn-icons-png.flaticon.com/512/733/733635.png' /></a>
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -30,7 +30,7 @@ function Header() {
                 <div className={isNavOpen ? 'block md:hidden' : 'hidden'}>
                     <div className="flex flex-col">
                         <div>
-                            <h1 className="text-md text-emerald-500">Blog-app</h1>
+                            <h1 className="text-lg text-emerald-500">Blog-app</h1>
                         </div>
                         <div className="flex flex-col">
                             <a href='/'>Home</a>
@@ -50,8 +50,8 @@ function Header() {
                     </div>
                     :
                     <div className="block md:hidden" onClick={() => handleClick()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 fill-black" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 fill-black" viewBox="0 0 448 512" stroke="currentColor">
+                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
                         </svg>
                     </div>
                 }
