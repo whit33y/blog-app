541e771fd9cd7f4d7c79fcf8da69a69cf6cbaa2b
whit33y <troppout@gmail.com>
2022-10-17 12:05:33 +0200

    React Querry installation and imports

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,31 @@ import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { AuthProvider } from './context/Auth';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import { queryClient } from './utils/queryClient';
+import { QueryClientProvider } from 'react-query';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/*' element={<NotFound />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/it' element={<ItCategory />} />
-          <Route path='/tech' element={<TechnologyCategory />} />
-          <Route path='/lifestyle' element={<LifestyleCategory />} />
-          <Route path='/accesstools' element={<Login />} />
-          <Route path='/signup' element={<ProtectedRoute><Signup /></ProtectedRoute>} />
-          <Route path='/health' element={<HealthCategory />} />
-          <Route path='/add' element={<ProtectedRoute><AddPost /></ProtectedRoute>} />
-          <Route path='/post/:id' element={<PostDetails />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/*' element={<NotFound />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/it' element={<ItCategory />} />
+            <Route path='/tech' element={<TechnologyCategory />} />
+            <Route path='/lifestyle' element={<LifestyleCategory />} />
+            <Route path='/accesstools' element={<Login />} />
+            <Route path='/signup' element={<ProtectedRoute><Signup /></ProtectedRoute>} />
+            <Route path='/health' element={<HealthCategory />} />
+            <Route path='/add' element={<ProtectedRoute><AddPost /></ProtectedRoute>} />
+            <Route path='/post/:id' element={<PostDetails />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
