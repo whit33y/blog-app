cf6a5f2647cc5fa1598ee45512b46e7718718473
whit33y <troppout@gmail.com>
2022-10-25 11:49:27 +0200

    Categories copying deleted issue #3 solved

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import './App.css';
 import { Home } from './pages/home/Home';
 import { About } from './pages/home/About'
 import { Contact } from './pages/home/Contact';
-import { ItCategory } from './pages/categories/ItCategory';
-import { TechnologyCategory } from './pages/categories/TechnologyCategory';
-import { LifestyleCategory } from './pages/categories/LifestyleCategory';
-import { HealthCategory } from './pages/categories/HealthCategory';
 import { AddPost } from './pages/post/AddPost';
 import { PostDetails } from './pages/post/PostDetails';
 import { NotFound } from './pages/NotFound';
@@ -17,6 +13,7 @@ import { AuthProvider } from './context/Auth';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { queryClient } from './utils/queryClient';
 import { QueryClientProvider } from 'react-query';
+import { RenderPostCategory } from './components/RenderPostCategory';
 
 function App() {
   return (
@@ -28,12 +25,12 @@ function App() {
             <Route path='/*' element={<NotFound />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
-            <Route path='/it' element={<ItCategory />} />
-            <Route path='/tech' element={<TechnologyCategory />} />
-            <Route path='/lifestyle' element={<LifestyleCategory />} />
+            <Route path='/it' element={<RenderPostCategory category='it' />} />
+            <Route path='/tech' element={<RenderPostCategory category='tech' />} />
+            <Route path='/lifestyle' element={<RenderPostCategory category='lifestyle' />} />
+            <Route path='/health' element={<RenderPostCategory category='health' />} />
             <Route path='/accesstools' element={<Login />} />
             <Route path='/signup' element={<ProtectedRoute><Signup /></ProtectedRoute>} />
-            <Route path='/health' element={<HealthCategory />} />
             <Route path='/add' element={<ProtectedRoute><AddPost /></ProtectedRoute>} />
             <Route path='/post/:id' element={<PostDetails />} />
           </Routes>
