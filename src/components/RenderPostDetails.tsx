dd2e3e167fcec77c6a648447136d39989f7417c4
whit33y <troppout@gmail.com>
2022-10-31 10:04:13 +0100

    Post details hook fix

--- src/components/RenderPostDetails.tsx
+++ src/components/RenderPostDetails.tsx
@@ -1,22 +1,20 @@
-import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { useGetPostDetails } from '../hooks/useGetPost'
 import { Link } from 'react-router-dom'
 function RenderPostDetails() {
   const { id } = useParams()
-  const getPost = useGetPostDetails(id)
-  const { isLoading, isError, data } = useQuery(['postDetails', id], () => getPost)
+  const post = useGetPostDetails(id)
   return (
     <div className='bg-gray-100 '>
-      {isLoading ? (
+      {post?.isLoading ? (
         <p className='text-2xl'>Loading ...</p>
-      ) : isError ? (
+      ) : post?.isError ? (
         <p className='text-2xl'>Error! </p>
-      ) : data ? (
+      ) : post?.data ? (
         <div className='flex flex-col sm:mx-0 md:mx-20'>
           <img
             className='object-scale-down h-80 w-auto bg-gray-100 mb-5'
-            src={data?.image}
+            src={post?.data?.image}
             alt='Post main'
           />
           <div>
@@ -27,14 +25,14 @@ function RenderPostDetails() {
               Back{' '}
             </Link>
             <h1 className='text-3xl text-sky-900'>
-              {data?.title}
+              {post?.data?.title}
               <span className='text-lg text-sky-900'>
                 <br />
-                {data?.category}
+                {post?.data?.category}
               </span>
             </h1>
             <p className='text-lg text-justify font-medium tracking-widest mb-3 text-sky-800'>
-              {data?.description}
+              {post?.data?.description}
             </p>
           </div>
         </div>
